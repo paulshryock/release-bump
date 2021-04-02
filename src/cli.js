8c01a82fbaf7f7b742f25b469b843d47c6a4425a
Paul Shryock <paul@pshry.com>
2021-04-02 19:44:55 -0400

    Release 1.1.0 (#6)

---
+++ src/cli.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+(function () {
+  const Bump = require('./bump.js')
+  new Bump()
+})()
---
+++ src/defaults.js
@@ -0,0 +1,15 @@
+/**
+ * Configuration defaults.
+ *
+ * @since 1.0.0
+ * @type  {Object}
+ */
+module.exports = {
+  changelog: {
+    filePath: './CHANGELOG.md',
+    gitRemote: 'github',
+    initialText: '',
+    initialTextUrl: 'https://keepachangelog.com/en/1.0.0/',
+    skipV: false,
+  },
+}
