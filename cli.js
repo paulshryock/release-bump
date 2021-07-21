ae46e5308a268dda3283322a38ea9b70f04e0db5
Paul Shryock <paul@pshry.com>
2021-07-21 09:46:22 -0400

    Add prefix configuration

--- cli.js
+++ cli.js
@@ -1,7 +1,30 @@
 #!/usr/bin/env node
 import Bump from './src/Bump.js'
+import meow from 'meow'
+
+const cli = meow(`
+  Usage
+    $ release-bump <input>
+
+  Options
+    --prefix, -p  Include a "v" prefix before the version number.
+
+  Examples
+    $ release-bump --prefix
+`, {
+  importMeta: import.meta,
+  flags: {
+    prefix: {
+      type: 'boolean',
+      alias: 'p',
+      default: false,
+      isMultiple: false,
+      isRequired: false,
+    }
+  }
+})
 
 ;(async function () {
-  const bump = new Bump()
+  const bump = new Bump({ ...cli.flags })
   await bump.init()
 })();
