254328feaf9a52d41f3e4ce67c4a5ce878699169
Paul Shryock (he/him) <paul@pshry.com>
2022-03-11 07:50:11 -0500

    Rewrite core api (#41)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,60 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: 'standard',
-  ignorePatterns: [
-    '.github',
-    'coverage',
-    'dist',
-  ],
-  /**
-   * @see:  https://github.com/tc39/proposal-private-methods
-   * @see:  https://tc39.es/proposal-private-methods/
-   * @todo: Remove @babel/eslint-parser when private methods reaches Stage 4.
-   *        - Remove `parser: '@babel/eslint-parser',`
-   *        - Remove `requireConfigFile: false,`
-   *        - `npm uninstall -D @babel/eslint-parser`
-   */
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    requireConfigFile: false,
-    sourceType: 'module',
-  },
-  rules: {
-    'comma-dangle': ['error', 'always-multiline'],
-    indent: ['error', 2, { offsetTernaryExpressions: true }],
-    'linebreak-style': ['error', 'unix'],
-    'max-len': ['error', { ignoreTemplateLiterals: true, ignoreStrings: true }],
-    'multiline-ternary': ['error', 'always-multiline'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-  },
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'standard',
+		'plugin:@typescript-eslint/recommended',
+	],
+	ignorePatterns: [
+		'coverage',
+		'dist',
+		'node_modules',
+		'tests',
+		'package-lock.json',
+		'tsconfig.json',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 13,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'json-format'],
+	rules: {
+		'@typescript-eslint/no-explicit-any': 'off',
+		'comma-dangle': ['error', 'always-multiline'],
+		'guard-for-in': 'error',
+		indent: [
+			'error',
+			'tab',
+			{
+				offsetTernaryExpressions: true,
+				SwitchCase: 1,
+			},
+		],
+		'max-len': [
+			'error',
+			{
+				code: 80,
+				ignoreRegExpLiterals: true,
+				ignoreStrings: true,
+				ignoreTemplateLiterals: true,
+				ignoreUrls: true,
+				tabWidth: 2,
+			},
+		],
+		'multiline-ternary': ['error', 'always-multiline'],
+		'no-tabs': 'off',
+		quotes: [
+			'error',
+			'single',
+			{ allowTemplateLiterals: true, avoidEscape: true },
+		],
+		'space-before-function-paren': [
+			'error',
+			{ anonymous: 'never', asyncArrow: 'always', named: 'never' },
+		],
+	},
 }
