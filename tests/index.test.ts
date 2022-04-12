7a0e5bf4c92688f3c7151139cb2e6bde71a044c0
Paul Shryock <pshryock@nba.com>
2022-04-12 18:00:58 -0400

    Add custom logger

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,5 @@
 import { releaseBump } from '../src/index.js'
+import { Logger } from '../src/lib.js'
 
 describe('releaseBump', () => {
 	describe('with fixtures', () => {
@@ -15,6 +16,25 @@ describe('releaseBump', () => {
 				'tests/fixtures/style.scss',
 			]
 			expect(actual).toEqual(expect.arrayContaining(expected))
+		})
+
+		describe('without quiet', () => {
+			test('logs to the console', async () => {
+				console.info = jest.fn()
+				const quiet = false
+				const loggerSpy = jest.spyOn(Logger({ quiet }), 'info')
+				await releaseBump({
+					changelogPath: 'tests/fixtures/changelog/CHANGELOG.md',
+					filesPath: 'tests/fixtures',
+					ignore: [],
+					quiet,
+					release: '3.0.0',
+				})
+				expect(loggerSpy).toHaveBeenCalledTimes(1)
+				expect(loggerSpy).toHaveBeenCalledWith(
+					expect.stringContaining('bumped'),
+				)
+			})
 		})
 	})
 
