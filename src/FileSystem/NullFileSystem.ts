19739e040147864c247edc7d7272b32ecb57ed09
Paul Shryock <paul@pshry.com>
2023-01-05 05:59:05 -0500

    work in progress

--- src/FileSystem/NullFileSystem.ts
+++ src/FileSystem/NullFileSystem.ts
@@ -6,17 +6,19 @@ export class NullFileSystem implements FileSystem {
 	}
 
 	async listFiles(): Promise<string[]> {
-		return []
+		return Promise.resolve([])
 	}
 
 	async readFile(file: string): Promise<string> {
 		this.#noop(file)
 
-		return JSON.stringify({ version: '' })
+		return Promise.resolve(JSON.stringify({ version: '' }))
 	}
 
 	async writeFile(path: string, data: string): Promise<void> {
 		this.#noop(path, data)
+
+		return Promise.resolve()
 	}
 
 	#noop(...x: Array<string | string[]>): Array<string | string[]> {
